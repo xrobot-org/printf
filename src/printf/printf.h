xrobot-org/printf

commit 87878b20a53ea25697cf35369a7a30174ecbd115

Regards #77: MISRA C:2004 Rule 19.10: In the definition of a function-like macro each instance of a parameter shall be enclosed in parentheses unless it is used as the operand of `#` or `##`.

src/printf/printf.h
@@ -51,9 +51,9 @@ extern "C" {
 #ifdef __GNUC__
 # define ATTR_PRINTF(one_based_format_index, first_arg) \
 __attribute__((format(__printf__, (one_based_format_index), (first_arg))))
-# define ATTR_VPRINTF(one_based_format_index) ATTR_PRINTF(one_based_format_index, 0)
+# define ATTR_VPRINTF(one_based_format_index) ATTR_PRINTF((one_based_format_index), 0)
 #else
-# define ATTR_PRINTF(one_based_format_index, first_arg)
+# define ATTR_PRINTF((one_based_format_index), (first_arg))
 # define ATTR_VPRINTF(one_based_format_index)
 #endif
 

commit 88eba51be738fd89437e736d343a0bd86e0dfe8f

Fixes #126: Corrects a bug introduced in commit 87878b2 for non-GNUC compilers, where macro arguments were mistakenly wrapped in parentheses.

src/printf/printf.h
@@ -53,7 +53,7 @@ extern "C" {
 __attribute__((format(__printf__, (one_based_format_index), (first_arg))))
 # define ATTR_VPRINTF(one_based_format_index) ATTR_PRINTF((one_based_format_index), 0)
 #else
-# define ATTR_PRINTF((one_based_format_index), (first_arg))
+# define ATTR_PRINTF(one_based_format_index, first_arg)
 # define ATTR_VPRINTF(one_based_format_index)
 #endif
 